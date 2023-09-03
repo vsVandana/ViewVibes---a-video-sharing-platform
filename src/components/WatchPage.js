1ca6cccf15e69b69aa29c8db04311c1439bf784f
vsVandana <vandananawhall@gmail.com>
2023-09-03 07:51:17 +0530

    Modify watchpage

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -10,7 +10,7 @@ const WatchPage = () => {
 
     useEffect(()=> {
         dispatch(closeMenu());
-    },[]);
+    },[dispatch]);
   return (
     <div className='py-9 px-20'>
         <iframe className="rounded-xl" width="660" height="400" src={"https://www.youtube.com/embed/"+searchParams.get("v")} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
